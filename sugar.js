bae3e3d13e90c8fd2e1cb41be90f5cdc499a2b73
Айгуль Давлетшина <ajguldavletsina@MacBook-Air-Ajgul.local>
2020-05-25 15:24:06 +0300

    cleaned

--- sugar.js
+++ sugar.js
@@ -1,32 +1,3 @@
-// function slider(selector) {
-//     let slider = $(selector);
-//     let imgs = slider.children();
-
-//   slider
-//   .addClass('slider')
-//   .append('<a href="#" class="slider_arrow slider_arrow_left"></a>')
-//   .append('<div class="slider_slides"></div>')
-//   .append('<div class="slider_dots"></div>')
-//   .append('<a href="#" class="slider_arrow slider_arrow_right"></a>');
-
-//   let slides = slider.children('.slider_slides');
-//   let dots = slider.children('.slider_dots');
-
-//   imgs
-//     .prependTo(slides)
-//     .each(function (i) {
-//         if (i) {
-//             dots.append('<a href= "#" class="slider_dot slider_dot_active"></a>');
-
-//         }else{
-//             dots.append('<a href= "#" class="slider_dot">');
-//         }
-//     })
-//     .addClass('slider_slide')
-//     .eq(0)
-//     .addClass('slider_slide_active');
-// }
-// slider('#slider');
 
 const buttonRight = document.getElementById('right-click');
 const buttonLeft = document.getElementById('left-click');
@@ -41,7 +12,7 @@ buttonRight.addEventListener('click', () => {
     slider.scrollTo({
         top: 0,
         left: (leftElemWidth + 30) * leftProductId,
-        behavior: 'smooth'
+        // behavior: 'smooth'
     });
 });
 
@@ -52,7 +23,7 @@ buttonLeft.addEventListener('click', () => {
         slider.scrollTo({
             top: 0,
             left: (leftElemWidth + 30) * leftProductId,
-            behavior: 'smooth'
+            // behavior: 'smooth'
         });
     }
 });
